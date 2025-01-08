fba6691ebf0c35bea1396361306892dd3d1cd32c
Murilo P. S <mu-silva@outlook.com>
2025-01-08 09:49:56 -0300

    feat: Adicionando projeto inicial

---
+++ src/main/java/com/github/amorixa/progressdocument/ProgressDocumentApplication.java
@@ -0,0 +1,13 @@
+package com.github.amorixa.progressdocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressDocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressDocumentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/amorixa/progressdocument/ProgressDocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.amorixa.progressdocument;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressDocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
